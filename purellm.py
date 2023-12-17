bf47c8fc7c49d2c0f6b17a5d2a9fd34222e461d0
Vishal Singh <persecvs@gmail.com>
2023-12-17 16:29:57 +0530

    simple llama-2 model

--- purellm.py
+++ purellm.py
@@ -1,14 +1,13 @@
-# from langchain.chat_models import ChatOpenAI
+from typing import List
+from pathlib import Path
 import os
 import yaml
 
-import chainlit as cl
-
-from langchain.llms import LlamaCpp, CTransformers
-from langchain.schema.runnable import Runnable
-from langchain.schema.runnable.config import RunnableConfig
+from langchain.llms import CTransformers
+from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, PromptTemplate
+
+import chainlit as cl
 
 import warnings
 
@@ -17,22 +16,57 @@ warnings.filterwarnings("ignore")
 
 def get_configs():
     with open(os.path.join(os.path.dirname(__file__), "config.yaml")) as infile:
-        config = yaml.safe_load(infile)
+        config_ = yaml.safe_load(infile)
         infile.close()
-    return config
+    return config_
 
 
-config = get_configs()
-model_path = config[["MODEL_PATH"]]
+local_config = get_configs()
+model_path = local_config["MODEL_PATH"]
 
-model = LlamaCpp(model_path=model_path, verbose=True)
+config = {
+    'max_new_tokens': 1024,
+    'repeatition_penalty': 1.1,
+    'temperature': 0,
+    'top_k': 50,
+    'top_p': 0.9,
+    'stream': True,
+    'threads': int(os.cpu_count() / 2)
+}
+
+llm_init = CTransformers(
+    model=model_path,
+    model_type='llama',
+    lib='avx2',
+    **config
+)
+
+template = """Question: {question}
+Let's think step by step and answer truthfully.
+"""
+
+
+@cl.on_chat_start
+def main():
+    prompt = PromptTemplate(
+        template=template, input_variables=["question"]
+    )
+    llm_chain = LLMChain(prompt=prompt, llm=llm_init, verbose=True)
+
+    # cache the use session
+    cl.user_session.set("llm_chain", llm_chain)
 
 
 @cl.on_message
-async def main(message: cl.Message):
-    # Your custom logic goes here...
+async def main(message: str):
+    # retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")
 
-    # Send a response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+    # call the chain asynchronously
+    results = await llm_chain.acall(
+        message,
+        callbacks=[cl.AsyncLangchainCallbackHandler()]
+    )
+
+    # return the result
+    await cl.Message(content=results["text"]).send()
